a35136e7e6d3ec305c68a6186f8e0200ddb7ea48
mdanieladla <mariadanieladarnea@gmail.com>
2021-10-29 18:06:06 +0200

    added js commented

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,39 @@
 'use strict';
 
 /*==================== MENU SHOW Y HIDDEN ====================*/
+//Traemos elementos del HTML
+const navMenu = document.querySelector('.js-nav-menu');
+
+const navToggle = document.querySelector('.js-nav-toggle');
+
+const navClose = document.querySelector('.js-nav-close');
 
 /*===== MENU SHOW =====*/
-/* Validate if constant exists */
+/* Validamos si (IF) la constante existe */
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
+}
 
 /*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
+/* Validamos si (IF) la constante existe */
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
+}
 
 /*==================== REMOVE MENU MOBILE ====================*/
+//Traemos elementos del HTML
+const navLink = document.querySelectorAll('.nav__link');
 
+function linkAction() {
+  const navMenu = document.querySelector('.js-nav-menu');
+  // Cuando hacemos click en cada nav__link, eliminamos la clase show-menu
+  navMenu.classList.remove('show-menu');
+}
+navLink.forEach((n) => n.addEventListener('click', linkAction));
 /*==================== ACCORDION SKILLS ====================*/
 
 /*==================== QUALIFICATION TABS ====================*/
