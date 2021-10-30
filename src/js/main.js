7c99e5e2b3a5cf0044dac00af90073622ac078ac
mdanieladla <mariadanieladarnea@gmail.com>
2021-10-30 21:12:15 +0200

    tried collapsable and dark mode but not working

--- src/js/main.js
+++ src/js/main.js
@@ -37,46 +37,28 @@ navLink.forEach((n) => n.addEventListener('click', linkAction));
 
 /*==================== ACCORDION SKILLS ====================*/
 //Traemos elementos del HTML
-// const skillsContent = document.querySelector('.js-skills-content');
+// const skillsContent = document.querySelectorAll('.js-skills');
 
 // const skillsHeader = document.querySelectorAll('.js-skills-header');
 
-// function toggleSkills() {
-//   let itemClass = this.parentNode.className;
-
-//   for (itemClass = 0; itemClass < skillsContent.length; itemClass++) {
-//     skillsContent[itemClass].className = 'skills__close js-skills-content';
-//   }
-//   if (itemClass === 'skills__close js-skills-content') {
-//     this.parentNode.className = 'skills__open js-skills-content';
+// function toggleSkills(ev) {
+//   let itemClass = ev.currentTarget;
+//   // console.log(itemClass);
+//   const clicked = itemClass.parentNode;
+//   for (let index = 0; index < skillsHeader.length; index++) {
+//     if (itemClass === skillsHeader[index]) {
+//       skillsContent[index].classList.toggle('skills__close');
+//       skillsHeader[index].classList.toggle('skills__header');
+//     } else {
+//       skillsContent[index].classList.add('skills__open');
+//     }
 //   }
 // }
 
-// skillsHeader.forEach((elemt) => {
-//   elemt.addEventListener('click', toggleSkills);
-// });
-
-//CÓDIGO IGUAL QUE EL DEL VIDEO, SE CIERRA PERO NO SE ABRE
-const skillsContent = document.getElementsByClassName('skills__content');
-
-const skillsHeader = document.querySelectorAll('.skills__header');
-
-function toggleSkills() {
-  let itemClass = this.parentNode.className;
-
-  for (itemClass = 0; itemClass < skillsContent.length; itemClass++) {
-    skillsContent[itemClass].className = 'skills__content skills__close';
-  }
-  if (itemClass === 'skills__content skills__close') {
-    this.parentNode.className = 'skills__content skills__open';
-  }
-}
-
-skillsHeader.forEach((elemt) => {
-  elemt.addEventListener('click', toggleSkills);
-});
-
-/*==================== QUALIFICATION TABS ====================*/
+// for (const item of skillsHeader) {
+//   item.addEventListener('click', toggleSkills);
+// }
+//    NO FUNCIONAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA    //
 
 /*==================== SERVICES MODAL ====================*/
 
@@ -85,9 +67,80 @@ skillsHeader.forEach((elemt) => {
 /*==================== TESTIMONIAL ====================*/
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+//Con esto hacemos que en el menú se quede sombreado en color morado claro la sección en que estamos.
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    let sectionId = current.getAttribute('id');
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.add('active-link');
+    } else {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.remove('active-link');
+    }
+  });
+}
+window.addEventListener('scroll', scrollActive);
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader() {
+  const nav = document.querySelector('.header');
+  //Cuando el scroll es mayor que 80 vh, añadimos la clase scroll-header al tag de header
+  if (this.scrollY >= 80) nav.classList.add('scroll-header');
+  else nav.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader);
 
 /*==================== SHOW SCROLL UP ====================*/
+function scrollUp() {
+  const scrollUp = document.getElementById('scroll-up');
+  //Cuando el scroll es mayor que 560vh, añadimos la clase show-scroll al tag <a> con la clase scroll-top
+  if (this.scrollY >= 560) scrollUp.classList.add('show-scroll');
+  else scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp);
 
 /*==================== DARK LIGHT THEME ====================*/
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'uil-sun';
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun';
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](
+    iconTheme
+  );
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem('selected-theme', getCurrentTheme());
+  localStorage.setItem('selected-icon', getCurrentIcon());
+});
